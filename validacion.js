f7f4c7c5a2040bebc6eb5050382068b59a9e8306
MarleneNaranjo1 <luna_satelites_estrellas@hotmail.com>
2024-07-15 21:58:47 -0600

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,5 @@
+// Aquí puedes agregar tus scripts JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("La página está completamente cargada.");
+    // Puedes añadir más funcionalidad aquí.
+});
